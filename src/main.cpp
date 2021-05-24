9f9f070625efbf009ca0fffad280c87ae41d9fcb
CesarSP99 <cesarandres00@gmail.com>
2021-05-24 03:34:44 -0400

    Datos en JSON y hora usando NTP

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <WiFiManager.h>
 #include <MAX30105.h>
 #include <heartRate.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <DateTime.h>
 
 MAX30105 particleSensor;
 
@@ -25,14 +29,23 @@ int noFinger;
 
 int avgIr;
 int avgTemp;
+int beatsok = 0;
 
 const String APIUrl = "";
+
+void setupDateTime() {
+  DateTime.setServer("time.google.com");
+  DateTime.setTimeZone("UTC");
+  DateTime.begin(5);
+  if (!DateTime.isTimeValid()) {
+    Serial.println("No se pudo obtener la fecha y hora del servidor");
+  }
+}
 
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Inicializando...");
-
   //Habilitando WiFi
   WiFi.mode(WIFI_STA);
   WiFiManager wm;
@@ -59,23 +72,33 @@ void setup()
   int pulseWidth = 411;
   int adcRange = 2048;
 
-  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
   
   particleSensor.setPulseAmplitudeRed(0x0A);
   particleSensor.setPulseAmplitudeGreen(0);
-  particleSensor.enableDIETEMPRDY(); //Enable the temp ready interrupt. This is required.
+  particleSensor.enableDIETEMPRDY();
+  pinMode(LED_BUILTIN, OUTPUT);
+  setupDateTime();
 }
 
 void mandarDatos(){
   HTTPClient http;
-  http.begin(APIUrl);
+  WiFiClient client;
+  http.begin(client, APIUrl);
   http.addHeader("Content-Type", "application/json");
-  //TODO: JSON de Datos
   String datos;
-  int respuesta = http.POST(datos);
-  String payload = http.getString();
-  Serial.print(respuesta);
-  Serial.println(": " + payload);
+  //TODO: Revisar el datetime
+  StaticJsonDocument<96> doc;
+  doc["idPaciente"] = 1;
+  doc["ritmoCardiaco"] = beatsPerMinute;
+  doc["saturacionOxigeno"] = perCent;
+  doc["fechaMedicion"] = DateTime.toUTCString();
+  serializeJson(doc, datos);
+  Serial.println(datos);
+  // int respuesta = http.POST(datos);
+  // String payload = http.getString();
+  // Serial.print(respuesta);
+  // Serial.println(": " + payload);
 }
 
 void loop()
@@ -84,7 +107,6 @@ void loop()
 
   if (checkForBeat(irValue) == true)
   {
-    //We sensed a beat!
     long delta = millis() - lastBeat;
     lastBeat = millis();
 
@@ -92,8 +114,8 @@ void loop()
 
     if (beatsPerMinute < 255 && beatsPerMinute > 20)
     {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
+      rates[rateSpot++] = (byte)beatsPerMinute;
+      rateSpot %= RATE_SIZE;
 
       //Take average of readings
       beatAvg = 0;
@@ -118,7 +140,7 @@ void loop()
   Serial.print("%");
   //Serial.print((float)samplesTaken / ((millis() - startTime) / 1000.0), 2);
 
-  float temperatureC = particleSensor.readTemperature(); //Because I am a bad global citizen
+  float temperatureC = particleSensor.readTemperature();
   temperatureC = temperatureC + degOffset;
   
   Serial.print(" Temp(C)=");
@@ -132,28 +154,38 @@ void loop()
     Serial.print(" No finger?");
     noFinger = noFinger+1;
   } else {
-    //only count and grab the reading if there's something there.
     count = count+1;
     avgIr = avgIr + irValue;
     avgTemp = avgTemp + temperatureC;
     Serial.print(" ... ");
   }
-
-  //Get an average IR value over 100 loops
   if (count == 100) {
     avgIr = avgIr / count;
     avgTemp = avgTemp / count;
     Serial.print(" avgO=");
     Serial.print(avgIr);
-    Serial.print(" avgF=");
+    Serial.print(" avgC=");
     Serial.print(avgTemp);
 
     Serial.print(" count=");
     Serial.print(count);
-    //reset for the next 100
     count = 0; 
     avgIr = 0;
     avgTemp = 0;
+  }
+  //Si se encuentra un bpm aceptable, se toman 10 muestras y se manda
+  if(beatsPerMinute>=60){
+    beatsok++;
+    if(beatsok == 30){  
+      if(perCent > 100)
+        perCent = 100;
+      digitalWrite(LED_BUILTIN, HIGH);
+      mandarDatos();
+      delay(20000);
+      digitalWrite(LED_BUILTIN, LOW);
+      beatsPerMinute = 0;
+      beatsok = 0;
+    }
   }
   Serial.println();
 }
